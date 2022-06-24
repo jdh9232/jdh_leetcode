5160f4bae234f4afbffa78793f2b58a87c5e02dd
jhh9232 <ghkd01212@gmail.com>
2022-06-24 14:11:27 +0900

    릿코드 템플릿 추가

---
+++ template/template.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+import unittest
+from typing import List
+
+
+"""
+Solution Note
+
+"""
+
+# Leet Code Solution
+
+class Solution:
+    def problem(self, input_data: List[int]) -> any:
+        return self.code(input_data)
+
+
+    def code(self, input_data: List[int]) -> any:
+        """ method description (python doxygen)
+
+        Args:
+            input_data (List[int]): input_data argument description
+
+        Returns:
+            any: If len(input_data) is 0 -> return None |
+            Else -> return input_data[0]
+        """
+        if (len(input_data) <= 0):
+            return None
+        output_data = input_data[0]
+        return output_data
+
+
+
+
+
+# python unit test
+class UnitTest(unittest.TestCase):
+
+    # 클래스 생성 시 1회 실행
+    @classmethod
+    def setUpClass(self):
+        self.solution = Solution();
+
+    # 클래스 소멸 시 1회 실행
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    # 테스트 케이스 전 실행
+    def setUp(self):
+        pass
+
+    # 테스트 케이스 후 실행
+    def tearDown(self):
+        pass
+
+    def test_case_example_normal_input_value(self):
+        res = self.solution.problem(input_data=[1])
+        self.assertEqual(res, 1)
+
+    def test_case_example_length_0(self):
+        res = self.solution.problem(input_data=[])
+        self.assertEqual(res, 1)
+
+
+
+if __name__ == "__main__":
+    # 스크립트 실행 시, 해당 부분 동작
+    unittest.main()
+    pass
