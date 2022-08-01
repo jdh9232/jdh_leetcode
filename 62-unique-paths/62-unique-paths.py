df682dc32db1c7661cf0958ed1594145da093158
jhh9232 <ghkd01212@gmail.com>
2022-08-01 13:05:25 +0900

    Time: 42 ms (71.26%), Space: 14 MB (14.98%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -25,33 +25,39 @@ class Solution:
             int: result of unique paths
         """
 
-        pos_dict : dict[(int, int)] = { }
+        pos_list = [[0] * n for _ in range(m)]
+
 
         def get_unique_paths(m: int, n: int) -> int:
             # pos_dict에 이미 값이 있으면 더이상 계산하지 않고 해당 값을 리턴한다.
-            if (m, n) in pos_dict:
-                return pos_dict[(m, n)]
+            if pos_list[m][n] != 0:
+                return pos_list[m][n]
 
             # 만약 m 또는 n이 1이면 최단경로는 1이므로
             # m이 2이면 n을 리턴하고
             # n이 2이면 m을 리턴하면 된다.
             # 이러면 굳이 한번 더 재귀를 타지 않아도 된다.
-            if m == 2:
-                pos_dict[(m, n)] = n
-                return n
-            if n == 2:
-                pos_dict[(m, n)] = m
-                return m
+            if m == 1:
+                pos_list[m][n] = n + 1
+                return n + 1
+            if n == 1:
+                pos_list[m][n] = m + 1
+                return m + 1
 
-            pos_dict[(m, n)] = get_unique_paths(m-1, n) + get_unique_paths(m, n-1)
-            return pos_dict[(m, n)]
+            pos_list[m][n] = get_unique_paths(m-1, n) + get_unique_paths(m, n-1)
+            return pos_list[m][n]
+
+        # 배열로 처리해주기 위하여 m,n을 하나씩 뺌
+        m -= 1
+        n -= 1
 
         # get_unique_paths(m, n) 에서
         # m 또는 n이 1일 경우에는 처리할 수 없는 로직이기에
         # 함수를 타기 전 예외처리 진행한다.
-        if m == 1 or n == 1:
+        if m == 0 or n == 0:
             return 1
 
         result = get_unique_paths(m, n)
         return result
+
 
