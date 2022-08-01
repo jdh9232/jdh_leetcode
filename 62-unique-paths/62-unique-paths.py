9c23ea971a30f59f41a2b4bd8b6b1f8cec66234d
jhh9232 <ghkd01212@gmail.com>
2022-08-01 13:26:38 +0900

    Time: 27 ms (98.36%), Space: 13.8 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,18 +1,3 @@
-"""
-Solution Note
-
-m = column
-n = row
-
-ex) m = 3, n = 4
-o o o o
-o o o o
-o o o o
-
-1 <= m, n <= 100
-"""
-
-# Leet Code Solution
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         """_summary_
@@ -25,39 +10,14 @@ class Solution:
             int: result of unique paths
         """
 
-        pos_list = [[0] * n for _ in range(m)]
+        grid = [[1] * n] * m
 
+        # 재귀를 for문으로 바꾸면 이렇게 된다.
+        # m - 1은 무조건 1 이므로 제외하고 시작한다.
+        # 0, 0 인덱스의 내용이 결과가 되도록 역순으로 for문을 진행한다.
+        for col in range(m-2, -1, -1):
+            for row in range(n-2, -1, -1):
+                grid[col][row] = grid[col+1][row] + grid[col][row+1]
 
-        def get_unique_paths(m: int, n: int) -> int:
-            # pos_dict에 이미 값이 있으면 더이상 계산하지 않고 해당 값을 리턴한다.
-            if pos_list[m][n] != 0:
-                return pos_list[m][n]
-
-            # 만약 m 또는 n이 1이면 최단경로는 1이므로
-            # m이 2이면 n을 리턴하고
-            # n이 2이면 m을 리턴하면 된다.
-            # 이러면 굳이 한번 더 재귀를 타지 않아도 된다.
-            if m == 1:
-                pos_list[m][n] = n + 1
-                return n + 1
-            if n == 1:
-                pos_list[m][n] = m + 1
-                return m + 1
-
-            pos_list[m][n] = get_unique_paths(m-1, n) + get_unique_paths(m, n-1)
-            return pos_list[m][n]
-
-        # 배열로 처리해주기 위하여 m,n을 하나씩 뺌
-        m -= 1
-        n -= 1
-
-        # get_unique_paths(m, n) 에서
-        # m 또는 n이 1일 경우에는 처리할 수 없는 로직이기에
-        # 함수를 타기 전 예외처리 진행한다.
-        if m == 0 or n == 0:
-            return 1
-
-        result = get_unique_paths(m, n)
-        return result
-
+        return grid[0][0]
 
