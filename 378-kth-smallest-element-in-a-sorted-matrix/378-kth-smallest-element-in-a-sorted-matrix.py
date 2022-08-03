d207f91d5f1c7d3f3e58be4a6a3e80394892223c
jhh9232 <ghkd01212@gmail.com>
2022-08-03 10:52:06 +0900

    정렬된 행렬이라는 특성을 통한 바이너리 서치 로직 알고리즘

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,6 +1,32 @@
+from bisect import bisect_right
+
 # Leet Code Solution
 class Solution:
+    # binary search solution
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        low, high = matrix[0][0], matrix[-1][-1]
+        # print("k = {}\n".format(k))
+        while (low < high):
+            mid = low + ((high - low) // 2)
+            # print("low = {}, high = {}, mid = {}".format(low, high, mid))
+            smaller_than_cnt = 0
+            for i in range(len(matrix)):
+                res = bisect_right(matrix[i], mid)
+                # print("[{}] - res : {}".format(i, res))
+                smaller_than_cnt += res
+                # smaller_than_cnt += bisect_right(matrix[i],mid)
+            # print("")
+            if smaller_than_cnt < k:
+                low = mid + 1
+            else:
+                high = mid
+
+        # print("")
+        # print("")
+        return low
+
+    # sort solution
+    def kthSmallest_sort(self, matrix: List[List[int]], k: int) -> int:
         lst_1dimension = []
         matrix_len_1d = len(matrix)
         for i in range(matrix_len_1d):
@@ -9,4 +35,3 @@ class Solution:
         
         lst_1dimension.sort()
         return lst_1dimension[k-1]
-
