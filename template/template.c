af111fc481c96d2e67f91c7440a303f66c5f19da
jhh9232 <ghkd01212@gmail.com>
2022-08-04 19:30:53 +0900

    자주 쓰는 define 추가

--- template/template.c
+++ template/template.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define bool unsigned char
+#define true 1
+#define false 0
+
+#define	ifree(_p) (((_p) != NULL) ? (free((_p)), (_p) = NULL) : ((void *)0))
+#define is_not_null(_ptr) ((_ptr) != NULL ? true : false)
+#define is_null(_ptr) ((_ptr) == NULL ? true : false)
+#define if_null_return(_ptr, _ret) if ((_ptr) == NULL) { return ret; }
+
+#define is_true(_c) (_c == true ? true : false)
+#define is_false(_c) (_c == true ? false : true)
+
+
+
 void solution()
 {
 	printf("Hello World\n");
