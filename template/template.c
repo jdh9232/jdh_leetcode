9cee84fedaf565061e434234cb42d463415bdf74
jhh9232 <ghkd01212@gmail.com>
2022-08-05 15:56:40 +0900

    변수명 오류 수정

--- template/template.c
+++ template/template.c
@@ -9,7 +9,7 @@
 #define	ifree(_p) (((_p) != NULL) ? (free((_p)), (_p) = NULL) : ((void *)0))
 #define is_not_null(_ptr) ((_ptr) != NULL ? true : false)
 #define is_null(_ptr) ((_ptr) == NULL ? true : false)
-#define if_null_return(_ptr, _ret) if ((_ptr) == NULL) { return ret; }
+#define if_null_return(_ptr, _ret) if ((_ptr) == NULL) { return _ret; }
 
 #define is_true(_c) (_c == true ? true : false)
 #define is_false(_c) (_c == true ? false : true)
