67dc1f8efdedb4883d382cd0822b25053f87d486
jhh9232 <ghkd01212@gmail.com>
2022-08-08 11:09:20 +0900

    Time: 77 ms (97.37%), Space: 14.3 MB (47.45%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,25 @@
+"""
+Solution Note
+
+1 <= nums.length <= 2500
+-10^4 <= nums[i] <= 10^4
+"""
+
+# Leet Code Solution
+from bisect import bisect_right
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [ nums[0] ]
+        for i in range(1, len(nums)):
+            if (nums[i] > lis[-1]):
+                lis.append(nums[i])
+                continue
+
+            index = bisect_left(lis, nums[i])
+            # index가 len(lis) 일 리는 없다 (num[i] > lis[-1]) 에서 처리했기 때문
+            if lis[index] == nums[i]:
+                continue
+            lis[index] = nums[i]
+
+        return len(lis)
