7f3362a933689477a18b9e87609c5b7ac6929f14
jhh9232 <ghkd01212@gmail.com>
2022-09-06 16:45:59 +0900

    릿코드 659 - fullcode

--- 659-split-array-into-consecutive-subsequences/fullcode.py
+++ 659-split-array-into-consecutive-subsequences/fullcode.py
@@ -16,7 +16,44 @@ nums is sorted in non-decreasing order.
 
 # Leet Code Solution
 class Solution:
+    # retry - 나중에 다시 풀어볼 것
     def isPossible(self, nums: List[int]) -> bool:
+        # https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/2447044/Python-and-C%2B%2BEasiest-approach-Explainedor-Dictionary-and-Map-or-Easy-understand-_
+
+        # print(nums)
+
+        seq = defaultdict(int)      # key: ending number, val: how many seqs
+        left = Counter(nums)        # key: number, val: how many of key are left unchecked
+        
+        for num in nums:
+            if (not left[num]): continue   # the number is already in seqs, we don't need to check again
+            # print(seq)
+            # print(left)
+            
+            if (seq[num-1] > 0):    # If there is sequence before the number, we add the number to the seq
+                seq[num-1] -= 1 
+                seq[num] += 1
+                left[num] -= 1
+                
+            else:   # If not we create a new seq using the number
+                if (not left[num+1] or not left[num+2]):  #  If there aren't two numbers behind to let us create new seq, return False
+                    # print("")
+                    # print("")
+                    return False
+                left[num] -= 1
+                left[num+1] -= 1
+                left[num+2] -= 1
+                seq[num+2] += 1
+
+        # print(seq)
+        # print(left)
+
+        # print("")
+        # print("")
+        return True
+
+
+    def isPossible1(self, nums: List[int]) -> bool:
         start = 0
         end = -1
         for i in range(len(nums) - 1):
@@ -64,29 +101,6 @@ class Solution:
         return True
 
 
-    def isPossible2(self, nums: List[int]) -> bool:
-        # https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/2447044/Python-and-C%2B%2BEasiest-approach-Explainedor-Dictionary-and-Map-or-Easy-understand-_
-        
-        seq = defaultdict(int)      # key: ending number, val: how many seqs
-        left = Counter(nums)        # key: number, val: how many of key are left unchecked
-        
-        for num in nums:
-            if (not left[num]): continue   # the number is already in seqs, we don't need to check again
-            
-            if (seq[num-1] > 0):    # If there is sequence before the number, we add the number to the seq
-                seq[num-1] -= 1 
-                seq[num] += 1
-                left[num] -= 1
-                
-            else:   # If not we create a new seq using the number
-                if (not left[num+1] or not left[num+2]):  #  If there aren't two numbers behind to let us create new seq, return False
-                    return False
-                left[num] -= 1
-                left[num+1] -= 1
-                left[num+2] -= 1
-                seq[num+2] += 1
-        
-        return True
 
 
 # python unit test
