7b14dcd7d0f082de3dc999efaf532642b1ffa462
jhh9232 <ghkd01212@gmail.com>
2022-11-22 08:58:53 +0900

    pytest 유닛테스트 코드 추가

--- template/leetcode_template.py
+++ template/leetcode_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import unittest
+import pytest
 from typing import List
 
 
@@ -9,63 +9,19 @@ Solution Note
 """
 
 # Leet Code Solution
-
 class Solution:
-    def problem(self, input_data: List[int]) -> any:
-        return self.code(input_data)
+    def problem(self, arg1, arg2) -> int:
+        return arg1 + arg2
 
 
-    def code(self, input_data: List[int]) -> any:
-        """ method description (python doxygen)
+@pytest.mark.parametrize(
+    'expect, args',
+    [
+        [3, (1, 2)]
+    ])
+def test_case_normal(expect, args):
+    solution = Solution();
+    print("test")
+    assert expect == solution.problem(args[0], args[1])
 
-        Args:
-            input_data (List[int]): input_data argument description
-
-        Returns:
-            any: If len(input_data) is 0 -> return None |
-            Else -> return input_data[0]
-        """
-        if (len(input_data) <= 0):
-            return None
-        output_data = input_data[0]
-        return output_data
-
-
-
-
-
-# python unit test
-class UnitTest(unittest.TestCase):
-
-    # 클래스 생성 시 1회 실행
-    @classmethod
-    def setUpClass(self):
-        self.solution = Solution();
-
-    # 클래스 소멸 시 1회 실행
-    @classmethod
-    def tearDownClass(self):
-        pass
-
-    # 테스트 케이스 전 실행
-    def setUp(self):
-        pass
-
-    # 테스트 케이스 후 실행
-    def tearDown(self):
-        pass
-
-    def test_case_normal_1(self):
-        res = self.solution.problem([1])
-        self.assertEqual(res, 1)
-
-    def test_case_normal_2(self):
-        res = self.solution.problem([2])
-        self.assertEqual(res, 1)
-
-
-
-if __name__ == "__main__":
-    # 스크립트 실행 시, 해당 부분 동작
-    unittest.main(verbosity=0)
-    pass
+# pytest fullcode.py
