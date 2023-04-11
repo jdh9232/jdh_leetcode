203f687bc978ea8f1f54a1f14669e4c28fe7ffe5
jhh9232 <ghkd01212@gmail.com>
2023-04-11 15:16:35 +0900

    cpp 템플릿 업데이트 (googletest)

--- template/leetcode_template.cpp
+++ template/leetcode_template.cpp
@@ -1,7 +1,10 @@
+#include <gtest/gtest.h>
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
+
 
 /*
  *
@@ -12,9 +15,41 @@ public:
 	string template_function() { return "template_function"; };
 };
 
-int main()
-{
-	Solution s = Solution();
 
-	cout << s.template_function() << endl;
+#define CLASS_NAME gtest_template
+
+class CLASS_NAME: public ::testing::Test {
+private:
+	clock_t t0;
+	clock_t t1;
+public:
+	Solution s;
+protected:
+	CLASS_NAME() { this->s = Solution(); }
+	~CLASS_NAME() override { }
+
+	void SetUp() override { set_start_time(); }
+	void TearDown() override { set_end_time(); get_duration_time(); }
+
+	void set_start_time() {
+		this->t0 = clock(); // or gettimeofday or whatever
+	}
+	void set_end_time() {
+		this->t1 = clock(); // or gettimeofday or whatever
+	}
+
+	// 시간 관련 체크
+	void get_duration_time() {
+		double elapsedSec = (double)(this->t1 - this->t0) / (double)1000000;
+		double elapsedMS = (double)(this->t1 - this->t0) / (double)1000;
+
+		cout << setprecision(3) << fixed << endl << "Duration of Times : ";
+		cout << elapsedMS << "ms, " << elapsedSec << "sec" << endl << endl;
+	}
+
+};
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+	EXPECT_EQ(this->s.template_function(), "template_function");
 }
