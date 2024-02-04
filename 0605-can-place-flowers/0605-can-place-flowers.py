c20258fd39057260a50f27e9ddccc294e23e8f80
Iblee <ghkd01212@gmail.com>
2024-02-04 20:15:59 +0900

    Time: 131 ms (77.29%), Space: 16.8 MB (75.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -13,19 +13,23 @@ class Solution:
                 return False
 
         for i in range(len(flowerbed)):
+            if flowerbed[i] == 1:
+                i += 1
+                continue
             if i == 0:
-                if flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                if flowerbed[i+1] == 0:
                     flowerbed[i] = 1
                     n -= 1
+                    i += 1
             elif i == last:
-                if flowerbed[i] == 0 and flowerbed[i-1] == 0:
+                if flowerbed[i-1] == 0:
                     flowerbed[i] = 1
                     n -= 1
             else:
-                if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                if flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
                     flowerbed[i] = 1
                     n -= 1
+                    i += 1
             if n == 0:
                 return True
         return False
-
