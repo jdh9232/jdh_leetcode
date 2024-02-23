d76e84d872e4098e567dc262162b0e0271fee6dc
jdh9232 <ghkd01212@gmail.com>
2024-02-23 23:27:36 +0900

    빌드 이후 모든 클래스 파일 제거

--- practice_code/leetcode_start.sh
+++ practice_code/leetcode_start.sh
@@ -5,16 +5,10 @@ cd "$curpath"
 
 JAVA_FILE="Solution.java"
 CLASS_NAME="${JAVA_FILE:0:${#JAVA_FILE}-5}"
-CLASS_FILE="${CLASS_NAME}.class"
-
-
-if [ -f "CLASS_FILE" ]; then
-    rm -f $CLASS_FILE
-fi
+rm -rf ./*.class
 
 javac $JAVA_FILE
 java $CLASS_NAME
 
-if [ -f "$CLASS_FILE" ]; then
-    rm -f $CLASS_FILE
-fi
+
+rm -rf ./*.class
