533c39503dc7da00546e074c2c19926d790e745e
Iblee <ghkd01212@gmail.com>
2024-02-24 15:42:33 +0900

    Time: 42 ms (88.24%), Space: 109.9 MB (44.12%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 class Solution {
 
     int[] groups;
@@ -11,48 +14,74 @@ class Solution {
     }
 
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        List<Integer> temp = new ArrayList<>();
-
         initializeGroups(n, firstPerson);
 
         // time 순으로 재정렬하기
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[2], b[2]));
 
         int timeIndex = 0;
-        // System.out.println("ago   groups: " + Arrays.toString(groups));
         while (timeIndex < meetings.length) {
-            int currentTime = meetings[timeIndex][2];
-            temp.clear();
-
-            while (timeIndex < meetings.length && meetings[timeIndex][2] == currentTime) {
-                int g1 = find(groups, meetings[timeIndex][0]);
-                int g2 = find(groups, meetings[timeIndex][1]);
-                groups[Math.max(g1, g2)] = Math.min(g1, g2);
-
-                temp.add(meetings[timeIndex][0]);
-                temp.add(meetings[timeIndex][1]);
-                ++timeIndex;
-            }
-
-            for (int j = 0; j < temp.size(); ++j) {
-                if (find(groups, temp.get(j)) != 0) {
-                    groups[temp.get(j)] = temp.get(j);
-                }
-            }
-            // System.out.println("after groups: " + Arrays.toString(groups));
+            timeIndex = traversalCurrentMettingTime(meetings, timeIndex);
         }
 
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (find(groups, i) == 0) result.add(i);
+        for (int person = 0; person < n; person++) {
+            // 0 이면 비밀을 알고 있는 것.
+            if (isKnowSecret(person)) {
+                result.add(person);
+            }
         }
 
         return result;
     }
 
-    private int find(int[] groups, int index) {
-        while (index != groups[index]) index = groups[index];
+    private int traversalCurrentMettingTime(int[][] meetings, int timeIndex) {
+        List<Integer> temp = new ArrayList<>();
+        int currentTime = meetings[timeIndex][2];
+
+        while (timeIndex < meetings.length) {
+            // 현재 시간과 같은 시간이 아니면 종료
+            if (meetings[timeIndex][2] != currentTime) {
+                break;
+            }
+
+            int g1 = find(meetings[timeIndex][0]);
+            int g2 = find(meetings[timeIndex][1]);
+            this.groups[Math.max(g1, g2)] = Math.min(g1, g2);
+
+            temp.add(meetings[timeIndex][0]);
+            temp.add(meetings[timeIndex][1]);
+            timeIndex++;
+        }
+
+        initialNoKnowSecretPerson(temp, timeIndex);
+        return timeIndex;
+    }
+
+    private void initialNoKnowSecretPerson(List<Integer> temp, int timeIndex) {
+        for (int i = 0; i < temp.size(); i++) {
+            int index = temp.get(i);
+            if (isKnowSecret(index) == false) {
+                this.groups[index] = index;
+            }
+        }
+    }
+
+    private int find(int index) {
+        // 0 0 0 0
+        // index = 3 groups[3] = 0
+        // index = 0 -> return 0
+        while (index != this.groups[index]) {
+            index = this.groups[index];
+        }
         return index;
+    }
+
+    private boolean isKnowSecret(int index) {
+        if (find(index) == 0) {
+            return true;
+        }
+        return false;
     }
 
 
