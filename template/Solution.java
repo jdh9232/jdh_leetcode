87da362879ee88bd2a75fd72e43c887813230086
jdh9232 <ghkd01212@gmail.com>
2024-02-24 14:46:36 +0900

    test case output 추가

--- template/Solution.java
+++ template/Solution.java
@@ -3,12 +3,23 @@
 class Solution {
 
 
+
+
+
     public static void main(String[] args) {
         testcase1();
     }
 
     public static void checkAnswer() {
         System.out.println("Hello checkAnswer!");
+
+        if (true) {
+            System.out.println("Testcase is Passed!");
+        } else {
+            System.out.println("@@@@@@@@@@ Testcase is Failed! @@@@@@@@@@");
+        }
+        System.out.println();
+
     }
 
     public static void testcase1() {
