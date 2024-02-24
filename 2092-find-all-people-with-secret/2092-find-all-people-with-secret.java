fbdd148e5ac7b54256ce4e0fbfd457f2717b02ab
Iblee <ghkd01212@gmail.com>
2024-02-24 15:14:13 +0900

    Time: 44 ms (85.29%), Space: 98.6 MB (73.53%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,35 +1,49 @@
 class Solution {
+
+    int[] groups = new int[100000];
+
+    private void initializeGroups(int n, int firstPerson) {
+        for (int i = 0; i < n; i++) {
+            groups[i] = i;
+        }
+        groups[firstPerson] = 0;
+    }
+
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        int[] groups = new int[100000];
-        List<Integer> result = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
 
-        for (int i = 0; i < n; ++i) groups[i] = i;
-        groups[firstPerson] = 0;
+        initializeGroups(n, firstPerson);
 
+        // time 순으로 재정렬하기
         Arrays.sort(meetings, (a, b) -> Integer.compare(a[2], b[2]));
 
-        int i = 0;
-        while (i < meetings.length) {
-            int currentTime = meetings[i][2];
+        int timeIndex = 0;
+        // System.err.println("ago   groups: " + Arrays.toString(groups));
+        while (timeIndex < meetings.length) {
+            int currentTime = meetings[timeIndex][2];
             temp.clear();
-            while (i < meetings.length && meetings[i][2] == currentTime) {
-                int g1 = find(groups, meetings[i][0]);
-                int g2 = find(groups, meetings[i][1]);
+
+            while (timeIndex < meetings.length && meetings[timeIndex][2] == currentTime) {
+                int g1 = find(groups, meetings[timeIndex][0]);
+                int g2 = find(groups, meetings[timeIndex][1]);
                 groups[Math.max(g1, g2)] = Math.min(g1, g2);
-                temp.add(meetings[i][0]);
-                temp.add(meetings[i][1]);
-                ++i;
+
+                temp.add(meetings[timeIndex][0]);
+                temp.add(meetings[timeIndex][1]);
+                ++timeIndex;
             }
+
             for (int j = 0; j < temp.size(); ++j) {
                 if (find(groups, temp.get(j)) != 0) {
                     groups[temp.get(j)] = temp.get(j);
                 }
             }
+            // System.err.println("after groups: " + Arrays.toString(groups));
         }
 
-        for (int j = 0; j < n; ++j) {
-            if (find(groups, j) == 0) result.add(j);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (find(groups, i) == 0) result.add(i);
         }
 
         return result;
@@ -39,4 +53,6 @@ class Solution {
         while (index != groups[index]) index = groups[index];
         return index;
     }
+
+
 }
